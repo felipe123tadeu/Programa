6ded7aef144544b10317331b6e74818f1b3d9751
Felipe <seu@email.com>
2017-04-13 18:09:29 -0300

    first commit

---
+++ 42.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ main(){
+	int n1, n2;
+	printf("digite um numero\n");
+	scanf("%d", &n1);
+	n2=n1%2;
+	if(n2==0)
+	{
+		printf("%d e par\n", n1);
+	}
+	else
+	{
+		printf("%d e impar\n", n1);
+		}
+	system("pause");
+		}
+	
+	
+	
+	
+ 
+ 
+ 
+ 
+ 
